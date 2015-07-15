7ac1cfd1cf6dae2b94985455a37025f708f486e7
takemitsu <takemitsu.suzuki@gmail.com>
2015-07-15 14:48:33 +0900

    login, register form cssなどを変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,43 +2,58 @@
 <html lang="ja">
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>login</title>
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="/css/admin.css">
 </head>
 <body>
 
-<h3>login</h3>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <h3>login</h3>
 
 @if (count($errors) > 0)
-    <div class="alert alert-danger">
+      <div class="alert alert-danger">
         <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
+    @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+    @endforeach
         </ul>
-    </div>
+      </div>
 @endif
 
-<form method="POST" action="/auth/login">
-{!! csrf_field() !!}
-<div>
-メールアドレス
-<input type="email" name="email" value="{{ old('email')}}">
+      <form class="form" method="POST" action="/auth/login">
+        {!! csrf_field() !!}
+        <div class="form-group">
+          <label>メールアドレス</label>
+          <input type="email" class="form-control" name="email" value="{{ old('email')}}">
+        </div>
+        <div class="form-group">
+          <label>パスワード</label>
+          <input type="password" class="form-control" name="password" id="password">
+        </div>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" name="remember"> ログインを継続する
+          </label>
+        </div>
+        <div>
+          <button type="submit" class="btn btn-primary">ログイン</button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <h3>register</h3>
+      <div class="form-group">
+        <a href="/auth/register" class="btn btn-default">新規ユーザ登録</a>
+      </div>
+    </div>
+  </div>
 </div>
-<div>
-パスワード
-<input type="password" name="password" id="password">
-</div>
-<div>
-<input type="checkbox" name="remember"> ログインを継続する
-</div>
-<div>
-<button type="submit">ログイン</button>
-</div>
-</form>
-<div>
-<a href="/auth/register">新規ユーザ登録</a>
-</div>
-
 </body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,45 +2,54 @@
 <html lang="ja">
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>register</title>
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="/css/admin.css">
 </head>
 <body>
 
-<h3>register</h3>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <h3>register</h3>
 
 @if (count($errors) > 0)
-    <div class="alert alert-danger">
+      <div class="alert alert-danger">
         <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+          <li>{{ $error }}</li>
             @endforeach
         </ul>
-    </div>
+      </div>
 @endif
 
-<form method="POST" action="/auth/register">
-{!! csrf_field() !!}
-<div>
-ユーザ名
-<input type="text" name="name" value="{{ old('name') }}">
+      <form method="POST" action="/auth/register">
+        {!! csrf_field() !!}
+        <div class="form-group">
+          <label>ユーザ名</label>
+          <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+        </div>
+        <div class="form-group">
+          <label>メールアドレス</label>
+          <input type="email" class="form-control" name="email" value="{{ old('email')}}">
+        </div>
+        <div class="form-group">
+          <label>パスワード</label>
+          <input type="password" class="form-control" name="password">
+        </div>
+        <div class="form-group">
+          <label>パスワード確認</label>
+          <input type="password" class="form-control" name="password_confirmation">
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">登録</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
-<div>
-メールアドレス
-<input type="email" name="email" value="{{ old('email')}}">
-</div>
-<div>
-パスワード
-<input type="password" name="password">
-</div>
-<div>
-パスワード確認
-<input type="password" name="password_confirmation">
-</div>
-<div>
-<button type="submit">登録</button>
-</div>
-</form>
 
 </body>
 </html>
